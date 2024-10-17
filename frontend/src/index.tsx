c6e08f295dd5a6925d983f56c6ab702c4137c424
YIMSEBIN <jij09123@gmail.com>
2024-10-17 17:33:26 +0900

    style: 폰트 global style 적용

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Global } from "@emotion/react";
+import { globalStyle } from "./styles/global";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
   <React.StrictMode>
+    <Global styles={globalStyle} />
     <App />
   </React.StrictMode>
 );
---
+++ frontend/src/styles/global.ts
@@ -0,0 +1,9 @@
+import { css } from "@emotion/react";
+import SpoqaHanSansNeoLight from "@assets/fonts/SpoqaHanSansNeo-Light.ttf";
+
+export const globalStyle = css`
+  @font-face {
+    font-family: "SpoqaHanSansNeoLight";
+    src: url(${SpoqaHanSansNeoLight}) format("truetype");
+  }
+`;
