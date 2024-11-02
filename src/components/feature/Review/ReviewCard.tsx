b3fb37b9c2286d96434dcd6e257e3b0e386446e0
YIMSEBIN <jij09123@gmail.com>
2024-11-03 04:21:47 +0900

    style: 리뷰 정렬

--- src/components/feature/Review/ReviewCard.tsx
+++ src/components/feature/Review/ReviewCard.tsx
@@ -1,7 +1,6 @@
 import Card from "@src/components/common/Card/Card";
 import { Pagination, styled } from "@mui/material";
 import { useState } from "react";
-import Typo from "@src/components/common/Typo/Typo";
 import { useGetReviews } from "@src/apis/hooks/useGetReviews";
 import Loading from "@src/components/common/Loading";
 import List from "@src/components/common/List/List";
@@ -44,6 +43,7 @@ export default function ReviewCard() {
     return (
       reviewListData && (
         <Card style={{ margin: "20px 20px", padding: "30px 30px", maxWidth: "1200px" }}>
+          <ReviewHeader />
           <List
             items={reviewListData}
             renderItem={(reviewData: ReviewDataProps) => <Item key={reviewData.reviewId} reviewData={reviewData} />}
@@ -65,22 +65,10 @@ function Item({ reviewData }: Props) {
   const { userName, date, content, rating } = reviewData;
   return (
     <ItemWrapper>
-      <TextWrapper>
-        <Typo>작성자</Typo>
-        <Typo>{userName}</Typo>
-      </TextWrapper>
-      <TextWrapper>
-        <Typo>작성날짜</Typo>
-        <Typo>{date.toString()}</Typo>
-      </TextWrapper>
-      <TextWrapper>
-        <Typo>별점</Typo>
-        <Typo>{rating}</Typo>
-      </TextWrapper>
-      <TextWrapper>
-        <Typo>내용</Typo>
-        <Typo>{content}</Typo>
-      </TextWrapper>
+      <TextWrapper width="25%">{userName}</TextWrapper>
+      <TextWrapper width="25%">{date.toString()}</TextWrapper>
+      <TextWrapper width="25%">{rating}</TextWrapper>
+      <TextWrapper width="25%">{content}</TextWrapper>
     </ItemWrapper>
   );
 }
@@ -89,5 +77,28 @@ const ItemWrapper = styled("div")(() => ({
   display: "flex",
   width: "100%",
   justifyContent: "space-between",
+  padding: "10px 0", // 아이템 간 간격을 위해 상하 패딩 추가
 }));
-const TextWrapper = styled("div")(() => ({}));
+const TextWrapper = styled("div")(({ width }: { width: string }) => ({
+  flex: "1",
+  maxWidth: width, // TextWrapper에 너비를 동적으로 할당
+  wordBreak: "break-word",
+  textAlign: "left", // 텍스트를 왼쪽 정렬
+}));
+
+function ReviewHeader() {
+  return (
+    <ItemWrapper
+      style={{
+        fontWeight: "bold", // 헤더 폰트 굵게
+        borderBottom: "2px solid #ccc", // 헤더 아래에 경계선 추가
+        paddingBottom: "5px", // 경계선과 텍스트 사이 간격
+      }}
+    >
+      <TextWrapper width="25%">작성자</TextWrapper>
+      <TextWrapper width="25%">작성날짜</TextWrapper>
+      <TextWrapper width="25%">별점</TextWrapper>
+      <TextWrapper width="25%">내용</TextWrapper>
+    </ItemWrapper>
+  );
+}
